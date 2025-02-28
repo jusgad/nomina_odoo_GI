9bf595ea44470434e46e3445fb944368639ef829
jusgad <jusga.adarve@gmail.com>
2025-02-28 16:33:19 -0500

    Actualizacion 2

--- wizards/__init__.py
+++ wizards/__init__.py
@@ -4,3 +4,5 @@ from . import hr_payroll_social_security_wizard
 from . import hr_payroll_electronic_wizard
 from . import hr_payroll_provision_wizard
 from . import hr_payroll_certificate_wizard
+from . import hr_pila_report_wizard
+from . import hr_severance_payment_wizard
